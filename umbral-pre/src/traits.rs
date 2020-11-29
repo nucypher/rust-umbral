471db9073e0c59e5142e5fcd3a326f336f742458
Bogdan Opanchuk <bogdan@opanchuk.net>
2020-11-28 20:22:44 -0800

    Add tests for SerializableToArray

--- umbral-pre/src/traits.rs
+++ umbral-pre/src/traits.rs
@@ -74,3 +74,113 @@ impl SerializableToArray for bool {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    use generic_array::sequence::Concat;
+    use generic_array::GenericArray;
+    use typenum::{op, U1, U2};
+
+    use super::SerializableToArray;
+
+    impl SerializableToArray for u8 {
+        type Size = U1;
+
+        fn to_array(&self) -> GenericArray<u8, Self::Size> {
+            GenericArray::<u8, Self::Size>::from([*self])
+        }
+
+        fn from_array(arr: &GenericArray<u8, Self::Size>) -> Option<Self> {
+            Some(arr.as_slice()[0])
+        }
+    }
+
+    impl SerializableToArray for u16 {
+        type Size = U2;
+
+        fn to_array(&self) -> GenericArray<u8, Self::Size> {
+            GenericArray::<u8, Self::Size>::from([(self >> 8) as u8, *self as u8])
+        }
+
+        fn from_array(arr: &GenericArray<u8, Self::Size>) -> Option<Self> {
+            let b1 = arr.as_slice()[0];
+            let b2 = arr.as_slice()[1];
+            Some(((b1 as u16) << 8) + (b2 as u16))
+        }
+    }
+
+    #[derive(Debug)]
+    struct SomeStruct {
+        f1: u16,
+        f2: u8,
+        f3: u16,
+        f4: bool,
+    }
+
+    impl PartialEq for SomeStruct {
+        fn eq(&self, other: &Self) -> bool {
+            self.f1 == other.f1 && self.f2 == other.f2 && self.f3 == other.f3 && self.f4 == other.f4
+        }
+    }
+
+    type U8Size = <u8 as SerializableToArray>::Size;
+    type U16Size = <u16 as SerializableToArray>::Size;
+    type BoolSize = <bool as SerializableToArray>::Size;
+
+    impl SerializableToArray for SomeStruct {
+        type Size = op!(U16Size + U8Size + U16Size + BoolSize);
+
+        fn to_array(&self) -> GenericArray<u8, Self::Size> {
+            self.f1
+                .to_array()
+                .concat(self.f2.to_array())
+                .concat(self.f3.to_array())
+                .concat(self.f4.to_array())
+        }
+
+        fn from_array(arr: &GenericArray<u8, Self::Size>) -> Option<Self> {
+            let (f1, rest) = u16::take(*arr)?;
+            let (f2, rest) = u8::take(rest)?;
+            let (f3, rest) = u16::take(rest)?;
+            let f4 = bool::take_last(rest)?;
+            Some(Self { f1, f2, f3, f4 })
+        }
+    }
+
+    #[test]
+    fn test_serialize() {
+        let s = SomeStruct {
+            f1: 1,
+            f2: 2,
+            f3: 3,
+            f4: true,
+        };
+        let s_arr = s.to_array();
+
+        let s_arr_ref: [u8; 6] = [0x00, 0x01, 0x02, 0x00, 0x03, 0x01];
+        assert_eq!(s_arr.as_slice(), &s_arr_ref);
+
+        let s_from_arr = SomeStruct::from_array(&s_arr).unwrap();
+        assert_eq!(s_from_arr, s);
+
+        let s_from_bytes = SomeStruct::from_bytes(&s_arr_ref).unwrap();
+        assert_eq!(s_from_bytes, s);
+    }
+
+    #[test]
+    fn test_invalid_data() {
+        // invalid value for `f4` (`bool` must be either 0 or 1)
+        let s_arr: [u8; 6] = [0x00, 0x01, 0x02, 0x00, 0x03, 0x02];
+        let s = SomeStruct::from_bytes(&s_arr);
+        assert!(s.is_none())
+    }
+
+    #[test]
+    fn test_invalid_length() {
+        // invalid value for `f4` (`bool` must be either 0 or 1)
+        let s_arr: [u8; 7] = [0x00, 0x01, 0x02, 0x00, 0x03, 0x01, 0x00];
+        let s = SomeStruct::from_bytes(&s_arr);
+        assert!(s.is_none())
+    }
+}
