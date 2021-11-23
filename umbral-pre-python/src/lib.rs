c4e5720e98176607d385decfc3f6e673d8dd2cc2
Bogdan Opanchuk <bogdan@opanchuk.net>
2021-11-22 22:48:40 -0800

    Move module population to umbral-pre-python

--- umbral-pre-python/src/lib.rs
+++ umbral-pre-python/src/lib.rs
@@ -1,10 +1,25 @@
 use pyo3::prelude::*;
 
-use umbral_pre;
+use umbral_pre::bindings_python::*;
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn _umbral(py: Python, m: &PyModule) -> PyResult<()> {
-    umbral_pre::bindings_python::build_module(py, m)?;
+    m.add_class::<SecretKey>()?;
+    m.add_class::<SecretKeyFactory>()?;
+    m.add_class::<PublicKey>()?;
+    m.add_class::<Signer>()?;
+    m.add_class::<Signature>()?;
+    m.add_class::<Capsule>()?;
+    m.add_class::<KeyFrag>()?;
+    m.add_class::<VerifiedKeyFrag>()?;
+    m.add_class::<CapsuleFrag>()?;
+    m.add_class::<VerifiedCapsuleFrag>()?;
+    m.add("VerificationError", py.get_type::<VerificationError>())?;
+    register_encrypt(m)?;
+    register_decrypt_original(m)?;
+    register_generate_kfrags(m)?;
+    register_reencrypt(m)?;
+    register_decrypt_reencrypted(m)?;
     Ok(())
 }
