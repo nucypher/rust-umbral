762e4b5f56e189c65ef895a31d230a0be02afd54
Bogdan Opanchuk <bogdan@opanchuk.net>
2022-09-19 15:39:10 -0700

    Remove wee_alloc - it's unmaintained

--- umbral-pre-wasm/src/lib.rs
+++ umbral-pre-wasm/src/lib.rs
@@ -1,11 +1,6 @@
 #![allow(clippy::unused_unit)] // Temporarily silence the warnings introduced in wasm-bindgen 0.2.79
 #![no_std]
 
-// Use `wee_alloc` as the global allocator.
-extern crate wee_alloc;
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
-
 extern crate alloc;
 
 pub use umbral_pre::bindings_wasm::*;
