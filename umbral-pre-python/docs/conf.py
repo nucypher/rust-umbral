74cfb3b1fcec5af3a85a7a4f2a29392aaa0e9187
Bogdan Opanchuk <bogdan@opanchuk.net>
2023-02-17 16:58:43 -0800

    Synchronize the dev version

--- umbral-pre-python/docs/conf.py
+++ umbral-pre-python/docs/conf.py
@@ -22,7 +22,7 @@ copyright = '2020, Bogdan Opanchuk'
 author = 'Bogdan Opanchuk'
 
 # The full version, including alpha/beta/rc tags
-release = '0.8.1'
+release = '0.9.0-dev'
 
 
 # -- General configuration ---------------------------------------------------
